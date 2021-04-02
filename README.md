ececa25706cf5d14afc03c9083782b07093a8f28
Riley <rileymshea@gmail.com>
2021-04-02 17:13:06 -0400

    start concurrency
    Signed-off-by: Riley <rileymshea@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# Learning: learn-go-with-tests
+
+Following along with https://github.com/quii/learn-go-with-tests
+
+Loving it so far!
+
+
--- mocking/main.go
+++ mocking/main.go
@@ -14,18 +14,37 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+type CountdownOperationsSpy struct {
+	Calls []string
 }
 
-type SpySleeper struct {
-	Calls int
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
 }
 
 func Countdown(out io.Writer, sleeper Sleeper) {
@@ -39,6 +58,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
