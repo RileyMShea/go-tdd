9c2270652252d4f513b5782cc9f11e16125972a5
Riley <rileymshea@gmail.com>
2021-04-02 13:14:56 -0400

    DI complete

--- di/di.go
+++ di/di.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
-func Greet(writer *bytes.Buffer, name string) {
+func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	// Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler)))
 }
